208745003632ffcc8bfbed4d69c6f542c0b4c44b
Ese Amadasun <amadasunese@gmail.com>
2023-10-05 14:44:02 +0100

    adding comment

--- 0x00-python_variable_annotations/0-add.py
+++ 0x00-python_variable_annotations/0-add.py
@@ -1,5 +1,12 @@
 #!/usr/bin/env python3
+"""
+Basic annotations - add
+"""
+
 
 def add(a: float, b: float) -> float:
+    """
+    Basic annotations function
+    """
     result = a + b
     return result
