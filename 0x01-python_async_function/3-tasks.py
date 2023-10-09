79cfec94910871fd8751a3a0973bad06bb2ddd11
Ese Amadasun <amadasunese@gmail.com>
2023-10-09 20:46:05 +0100

    tasks

---
+++ 0x01-python_async_function/3-tasks.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+""" Async function returns a asyncio """
+
+import asyncio
+wait_random = __import__('0-basic_async_syntax').wait_random
+
+
+def task_wait_random(max_delay: int) -> asyncio.Task:
+    """ Async function returns a asyncio"""
+    return asyncio.create_task(wait_random(max_delay))
