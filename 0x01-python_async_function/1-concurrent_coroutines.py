4506f12ec24b086782331b2db1370d5a5f8f1040
Ese Amadasun <amadasunese@gmail.com>
2023-10-09 13:27:00 +0100

    multiple coroutines

--- 0x01-python_async_function/1-concurrent_coroutines.py
+++ 0x01-python_async_function/1-concurrent_coroutines.py
@@ -1,17 +1,37 @@
 #!/usr/bin/env python3
-"""
-Multiple coroutines at the same time with async
-"""
+
 import asyncio
-from typing import List
+import random
 
-wait_random = __import__('0-basic_async_syntax').wait_random
-
-
-async def wait_n(n, max_delay=10):
+async def wait_random(max_delay: int = 10) -> float:
     """
-    multiple coroutines at the same time with async
+    The basics of async
     """
+    delay = random.uniform(0, max_delay)
+    await asyncio.sleep(delay)
+    return delay
+
+async def wait_n(n, max_delay):
+    # Create a list to store the delays
+    delays = []
+
+    # Create a list of asyncio tasks for wait_random
     tasks = [wait_random(max_delay) for _ in range(n)]
-    delays = await asyncio.gather(*tasks)
-    return sorted(delays)
+
+    # Execute all tasks concurrently using asyncio.gather
+    results = await asyncio.gather(*tasks)
+
+    # Sort the results in ascending order
+    delays = sorted(results)
+
+    return delays
+
+async def main():
+    n = 5
+    max_delay = 10
+
+    delays = await wait_n(n, max_delay)
+
+    print("Delays in ascending order:")
+    for delay in delays:
+        print(f"{delay:.2f} seconds")
