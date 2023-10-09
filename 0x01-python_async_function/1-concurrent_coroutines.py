c24ac1321e1b3924c645246a08cc95c4c5c723e9
Ese Amadasun <amadasunese@gmail.com>
2023-10-09 13:43:42 +0100

    updated

--- 0x01-python_async_function/1-concurrent_coroutines.py
+++ 0x01-python_async_function/1-concurrent_coroutines.py
@@ -14,27 +14,12 @@ async def wait_random(max_delay: int = 10) -> float:
 
 
 async def wait_n(n: int, max_delay: int) -> float:
-    # Create a list to store the delays
     delays = []
 
-    # Create a list of asyncio tasks for wait_random
     tasks = [wait_random(max_delay) for _ in range(n)]
 
-    # Execute all tasks concurrently using asyncio.gather
     results = await asyncio.gather(*tasks)
 
-    # Sort the results in ascending order
     delays = sorted(results)
 
     return delays
-
-
-async def main():
-    n = 5
-    max_delay = 10
-
-    delays = await wait_n(n, max_delay)
-
-    print("Delays in ascending order:")
-    for delay in delays:
-        print(f"{delay:.2f} seconds")
