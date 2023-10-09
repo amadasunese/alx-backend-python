e3271083f80be6850dfc4e9f459b19fe9f8e5780
Ese Amadasun <amadasunese@gmail.com>
2023-10-09 16:03:30 +0100

    updated

--- 0x01-python_async_function/1-concurrent_coroutines.py
+++ 0x01-python_async_function/1-concurrent_coroutines.py
@@ -2,15 +2,9 @@
 """The basics of async"""
 import asyncio
 import random
+from typing import List
 
-
-async def wait_random(max_delay: int = 10) -> float:
-    """
-    The basics of async
-    """
-    delay = random.uniform(0, max_delay)
-    await asyncio.sleep(delay)
-    return delay
+wait_random = __import__('0-basic_async_syntax').wait_random
 
 
 async def wait_n(n: int, max_delay: int) -> float:
