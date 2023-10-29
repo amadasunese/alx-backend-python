12df3110172a5276090c8fda56c45847d96f120b
Ese Amadasun <amadasunese@gmail.com>
2023-10-29 19:35:50 +0100

    fix pycodestyle error

--- 0x03-Unittests_and_integration_tests/test_utils.py
+++ 0x03-Unittests_and_integration_tests/test_utils.py
@@ -56,7 +56,8 @@ class TestMemoize(unittest.TestCase):
             def a_property(self):
                 return self.a_method()
 
-        with patch.object(TestClass, 'a_method', return_value=42) as mock_method:
+        with patch.object(TestClass, 'a_method',
+                          return_value=42) as mock_method:
             test_class_instance = TestClass()
             self.assertEqual(test_class_instance.a_property, 42)
             self.assertEqual(test_class_instance.a_property, 42)
